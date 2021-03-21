8c18b7ce03b581e0fbf5b08414f55ba5ccd14719
Mohamed Shahat <mshahat@pivotal.io>
2021-03-21 18:08:22 +0000

    Add txtPrefix
    Added txtprefix to avoid an issue with Google DNS where it doesn't allow TXT and A records of the same name

--- scripts/setup-edukates.sh
+++ scripts/setup-edukates.sh
@@ -3,7 +3,7 @@ VALUES_YAML=${1:-values.yaml}
 
 EDUKATES_TAG=20.12.03.1
 
-export KUBECONFIG=generated/kubeconfig-worker
+#export KUBECONFIG=generated/kubeconfig-worker
 
 kapp deploy -a edukates \
   -f <(kubectl apply -k "github.com/eduk8s/eduk8s?ref=$EDUKATES_TAG" --dry-run=client -o yaml | \
